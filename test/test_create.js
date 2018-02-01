f1f68be726194ec692ee1f80af8565133d4ce807
Bacra Woo <bacra.woo@gmail.com>
2018-02-02 01:31:33 +0800

    Add: output of test

--- test/test_create.js
+++ test/test_create.js
@@ -1,10 +1,17 @@
+var fs = require('fs');
+var _ = require('lodash');
 var inputData = require('./input.json');
 var Creator = require('../lib/create');
 
-// console.log(inputData);
-console.log(Creator.create(inputData,
+var output = Creator.create(inputData,
 	{
 		title: '第一份翻译稿v1.0',
 		email: 'bacra.woo@gmail.com',
 		pickFileLanguages: ['en-US']
-	}));
+	});
+
+fs.writeFileSync(__dirname+'/output/all.po', output.po);
+_.each(output.pot, function(content, filename)
+{
+	fs.writeFileSync(__dirname+'/output/'+filename+'.pot', content);
+});
